78ec49d0d2f42f860fd6af75d353fc1282f26e76
Matthew <matthewgeorgy61@gmail.com>
2021-10-11 14:34:25 -0400

    PUSH v0.2.6dll

--- dll/inc/dllmain.h
+++ dll/inc/dllmain.h
@@ -1,6 +1,7 @@
 /*
     Version History
 
+		0.2.6	Added more winuser functions
         0.2.5   Added processthreadsapi functions to the patch list
         0.2.4   Added more functions to wt_processthreadsapi
         0.2.3   Added common.c and ShowDetails() to display trace opts
