b3475a72f40f89094e21719b72af13ace541148b
Matthew <matthewgeorgy61@gmail.com>
2021-11-07 12:46:27 -0500

    add test_fileapi + update make + testscripts

--- make.bat
+++ make.bat
@@ -28,6 +28,7 @@ goto :EOF
 :test
     pushd build\tests
         cl /W4 /MD w:\wintrace\tests\test_heapapi.c
+        cl /W4 /MD w:\wintrace\tests\test_fileapi.c
     popd
 goto :EOF
 
@@ -36,7 +37,8 @@ goto :EOF
         cl /W4 /MP /LD /Fe"wintrace.dll" w:\wintrace\dll\*.c user32.lib kernel32.lib advapi32.lib
         cl /W4 /MP /Fe"wintrace.exe" w:\wintrace\core\*.c kernel32.lib
         pushd tests
-            cl /W4 /MD w:\wintrace\tests\test_heapapi.c
+			cl /W4 /MD w:\wintrace\tests\test_heapapi.c
+			cl /W4 /MD w:\wintrace\tests\test_fileapi.c
         popd
     popd
 goto :EOF
--- runtests.bat
+++ runtests.bat
@@ -2,4 +2,5 @@
 
 pushd build
 wintrace.exe /c /t /p tests\test_heapapi.exe
+wintrace.exe /c /t /p tests\test_fileapi.exe
 popd
