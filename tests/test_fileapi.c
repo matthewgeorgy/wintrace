501af2f1a518df17a2665d22fb953278db93ebc5
Matthew <matthewgeorgy61@gmail.com>
2021-12-01 17:15:54 -0500

    updated errors in test_fileapi

--- tests/test_fileapi.c
+++ tests/test_fileapi.c
@@ -5,34 +5,35 @@
 int
 main(void)
 {
-	BOOL		Status;
-	HANDLE		File;
-	DWORD		Written, Read, Offset;
-	char		*MyStringBuffer = "Hello World!\r\n";
-	char		MyBuffer[200];
+    BOOL        Status;
+    HANDLE      File;
+    DWORD       Written, Read, Offset;
+    char        *MyStringBuffer = "Hello World!\r\n";
+    char        MyBuffer[200];
 
 
     printf("\n|------------TEST: %s------------|\n", __FILE__);
-	Status = CreateDirectoryA("Foo", NULL);
-	File = CreateFileA("Foo\\bar.txt", GENERIC_READ | GENERIC_WRITE, 0, NULL, CREATE_ALWAYS, FILE_ATTRIBUTE_NORMAL, NULL);
-	Status = WriteFile(File, MyStringBuffer, (DWORD)strlen(MyStringBuffer), &Written, NULL);
-	Offset = SetFilePointer(File, 0, 0, FILE_BEGIN);
-	Status = ReadFile(File, MyBuffer, sizeof(MyBuffer) - 1, &Read, NULL);
-	Status = DeleteFileA("Foo\\bar.txt");
-	Status = RemoveDirectoryA("Foo");
-	CloseHandle(File);
+    Status = CreateDirectoryA("Foo", NULL);
+    File = CreateFileA("Foo\\bar.txt", GENERIC_READ | GENERIC_WRITE, 0, NULL, CREATE_ALWAYS, FILE_ATTRIBUTE_NORMAL, NULL);
+    Status = WriteFile(File, MyStringBuffer, (DWORD)strlen(MyStringBuffer), &Written, NULL);
+    Offset = SetFilePointer(File, 0, 0, FILE_BEGIN);
+    Status = ReadFile(File, MyBuffer, sizeof(MyBuffer) - 1, &Read, NULL);
+    CloseHandle(File);
+    Status = DeleteFileA("Foo\\bar.txt");
+    Status = RemoveDirectoryA("Foo");
 
-	printf("\n");
-	Status = CreateDirectoryW(L"Foo", NULL);
-	File = CreateFileW(L"Foo\\bar.txt", GENERIC_READ | GENERIC_WRITE, 0, NULL, CREATE_ALWAYS, FILE_ATTRIBUTE_NORMAL, NULL);
-	Status = WriteFile(File, MyStringBuffer, (DWORD)strlen(MyStringBuffer), &Written, NULL);
-	Offset = SetFilePointer(File, 0, 0, FILE_BEGIN);
-	Status = ReadFile(File, MyBuffer, sizeof(MyBuffer) - 1, &Read, NULL);
-	Status = DeleteFileW(L"Foo\\bar.txt");
-	Status = RemoveDirectoryW(L"Foo");
-	CloseHandle(File);
+    printf("\n");
+    Status = CreateDirectoryW(L"Foo", NULL);
+    File = CreateFileW(L"Foo\\bar.txt", GENERIC_READ | GENERIC_WRITE, 0, NULL, CREATE_ALWAYS, FILE_ATTRIBUTE_NORMAL, NULL);
+    Status = WriteFile(File, MyStringBuffer, (DWORD)strlen(MyStringBuffer), &Written, NULL);
+    Offset = SetFilePointer(File, 0, 0, FILE_BEGIN);
+    Status = ReadFile(File, MyBuffer, sizeof(MyBuffer) - 1, &Read, NULL);
+    // Intentionally fail since the file handle has not been closed yet
+    Status = DeleteFileW(L"Foo\\bar.txt");
+    Status = RemoveDirectoryW(L"Foo");
+    CloseHandle(File);
     printf("|-----------------------------|\n\n");
 
-	return 0;
+    return 0;
 }
 
