fd8cafe011212836c9abd0c8beb69c5fd5c0d5d0
matthewgeorgy <matthewgeorgy61@gmail.com>
2025-04-18 19:14:45 -0400

    updated make.bat to link with advapi32.lib (process functions)

--- build/make.bat
+++ build/make.bat
@@ -1,5 +1,5 @@
 @echo off
 
-cl /W4 /MP /LD /Fe"wintrace.dll" w:\wintrace\dll\*.c user32.lib kernel32.lib
+cl /W4 /MP /LD /Fe"wintrace.dll" w:\wintrace\dll\*.c user32.lib kernel32.lib advapi32.lib
 
 cl /W4 /MP /Fe"wintrace.exe" w:\wintrace\core\*.c kernel32.lib
