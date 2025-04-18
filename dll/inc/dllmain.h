a246e89f64e9c6474ef863686e446fb90efe46e8
matthewgeorgy <matthewgeorgy61@gmail.com>
2025-04-18 19:12:14 -0400

    v0.2.4dll: added more functions to wt_processthreadsapi

--- dll/inc/dllmain.h
+++ dll/inc/dllmain.h
@@ -1,6 +1,7 @@
 /*
     Version History
 
+        0.2.4   Added more functions to wt_processthreadsapi
         0.2.3   Added common.c and ShowDetails() to display trace opts
         0.2.2   Added procID/threadID tracing
         0.2.1   Function call counts starts at 1 and enclosed in ()
