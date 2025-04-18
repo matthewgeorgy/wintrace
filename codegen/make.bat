0e0bbf317b3dc6b085213440eab1aba648814d04
matthewgeorgy <matthewgeorgy61@gmail.com>
2025-04-18 18:58:12 -0400

    ----- STARING OVER; MIGRATING SVN HISTORY TO GIT -----

--- codegen/make.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-
-cl hash.c
-hash.exe
-cl /W4 /Zi /MP /Fe"hookgen" main.c user32.lib kernel32.lib
--- dll/inc/win32/wt_profileapi.h
+++
@@ -1,9 +0,0 @@
-#ifndef WT_PROFILEAPI_H
-#define WT_PROFILEAPI_H
-
-#include "common.h"
-
-BOOL WtQueryPerformanceCounter(PLARGE_INTEGER lpPerformanceCount);
-BOOL WtQueryPerformanceFrequency(PLARGE_INTEGER lpFrequency);
-
-#endif // WT_PROFILEAPI_H
