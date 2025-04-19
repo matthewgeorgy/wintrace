eac85ac84a8e5323f2e97786aa13600cb627c55c
matthewgeorgy <matthewgeorgy61@gmail.com>
2025-04-18 20:10:42 -0400

    hookgen now generates all the hooks at once

--- hookgen/compile.bat
+++ hookgen/compile.bat
@@ -1,14 +1,8 @@
 @echo off
 
-:: Win32 hooks
-hookgen wt_debugapi Wt win32
-hookgen wt_fileapi Wt win32
-hookgen wt_heapapi Wt win32
-hookgen wt_memoryapi Wt win32
-hookgen wt_processthreadsapi Wt win32
-hookgen wt_profileapi Wt win32
-hookgen wt_winuser Wt win32
+hookgen
 
+:: Win32 hooks
 mv wt_debugapi.h W:\wintrace\dll\inc\win32
 mv wt_fileapi.h W:\wintrace\dll\inc\win32
 mv wt_heapapi.h W:\wintrace\dll\inc\win32
@@ -26,9 +20,6 @@ mv wt_profileapi.c W:\wintrace\dll\win32
 mv wt_winuser.c W:\wintrace\dll\win32
 
 :: CRT hooks
-hookgen wt_stdio wt_ crt
-hookgen wt_stdlib wt_ crt
-
 mv wt_stdio.h W:\wintrace\dll\inc\crt
 mv wt_stdlib.h W:\wintrace\dll\inc\crt
 
--- hookgen/main.c
+++ hookgen/main.c
@@ -4,6 +4,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdarg.h>
+#include <assert.h>
 #include "hashes.h"
 
 #define BUFF_SIZE		1024 * 1024 // 1 MB
@@ -23,42 +24,311 @@ typedef struct _tag_Buffer
 	SIZE_T		Pos;
 } T_Buffer;
 
+CHAR *HookList[] =
+{
+	"wt_debugapi Wt win32",
+	"wt_fileapi Wt win32",
+	"wt_heapapi Wt win32",
+	"wt_memoryapi Wt win32",
+	"wt_processthreadsapi Wt win32",
+	"wt_profileapi Wt win32",
+	"wt_winuser Wt win32",
+	"wt_stdio wt_ crt",
+	"wt_stdlib wt_ crt"
+};
+
 CHAR			**StringFile(char *filename, int *plen);
 T_Function		*ParseFunctions(CHAR *Filename, INT *Count);
 void			WriteBuffer(T_Buffer *Buffer, char *Format, ...);
 void			GetFormat(CHAR *Format, CHAR *Type);
 DWORD			Djb2(LPSTR String);
+void			GenerateHooks(T_Function *Functions, INT Count, CHAR *ListName, CHAR *Prefix, CHAR *Folder);
 
 int
-main(int argc,
-	 char **argv)
+main(void)
+{
+	T_Function		*Functions[9];
+	INT				Count[9],
+					I;
+	CHAR			ListName[32],
+					Prefix[32],
+					Folder[32];
+
+	
+	assert(_countof(Functions) == _countof(HookList));	
+	assert(_countof(Count) == _countof(HookList));	
+
+	for (I = 0; I < _countof(HookList); I++)
+	{
+		sscanf(HookList[I], "%s %s %s", ListName, Prefix, Folder);
+
+		Functions[I] = ParseFunctions(ListName, &Count[I]);
+
+		GenerateHooks(Functions[I], Count[I], ListName, Prefix, Folder);
+	}
+
+	return (0);
+}
+
+T_Function *
+ParseFunctions(CHAR *Filename,
+			   INT *Count)
 {
 	T_Function		*Functions;
-	INT				Count,
-					Len;
+	CHAR			**File,
+					*Signature,
+					*Temp;
+	INT				Len;
+
+
+	File = StringFile(Filename, &Len);
+	Functions = (T_Function *)calloc(1, Len * sizeof(*Functions));
+	*Count = Len;
+
+	for (INT J = 0; J < Len; J++)
+	{
+		Signature = File[J];
+
+		// Type
+		Temp = strtok(Signature, " ,()");
+		memcpy(Functions[J].ReturnType, Temp, strlen(Temp));
+
+		// Name
+		Temp = strtok(NULL, "(");
+		memcpy(Functions[J].Name, Temp, strlen(Temp));
+
+		// Arguments
+		INT Arg = 0;
+		while (Temp != NULL)
+		{
+			Temp = strtok(NULL, " ");
+			if (!Temp)
+				break;
+			memcpy(Functions[J].ArgTypes[Arg], Temp, strlen(Temp));
+
+			Temp = strtok(NULL, ",)");
+			if (!Temp)
+				break;
+			memcpy(Functions[J].ArgNames[Arg], Temp, strlen(Temp));
+
+			Arg++;
+		}
+		Functions[J].ArgCount = Arg;
+	}
+
+	return (Functions);
+}
+
+// stb_stringfile, thank you Sean Barrett
+CHAR **
+StringFile(char *filename, int *plen)
+{
+	HANDLE				f;
+	char				*buffer, **list = NULL, *s;
+	size_t				len, count, i;
+	LARGE_INTEGER		large_int;
+
+	f = CreateFile(filename, GENERIC_READ, 0, NULL, OPEN_ALWAYS, FILE_ATTRIBUTE_NORMAL, NULL);
+	if (!f) return NULL;
+	GetFileSizeEx(f, &large_int);
+	len = large_int.QuadPart;
+	buffer = (char *)HeapAlloc(GetProcessHeap(), 0, len + 1);
+	ReadFile(f, buffer, (DWORD)len, (LPDWORD)&len, NULL);
+	buffer[len] = 0;
+	CloseHandle(f);
+
+   // two passes through: first time count lines, second time set them
+   for (i=0; i < 2; ++i) {
+      s = buffer;
+      if (i == 1)
+         list[0] = s;
+      count = 1;
+      while (*s) {
+         if (*s == '\n' || *s == '\r') {
+            // detect if both cr & lf are together
+            int crlf = (s[0] + s[1]) == ('\n' + '\r');
+            if (i == 1) *s = 0;
+            if (crlf) ++s;
+            if (s[1]) {  // it's not over yet
+               if (i == 1) list[count] = s+1;
+               ++count;
+            }
+         }
+         ++s;
+      }
+      if (i == 0) {
+         list = (char **)HeapAlloc(GetProcessHeap(), 0, sizeof(*list) * (count+1) + len+1);
+         if (!list) return NULL;
+         list[count] = 0;
+         // recopy the file so there's just a single allocation to free
+         memcpy(&list[count+1], buffer, len+1);
+         HeapFree(GetProcessHeap(), 0, buffer);
+         buffer = (char *) &list[count+1];
+         if (plen) *plen = (int) count;
+      }
+   }
+   return list;
+}
+
+void
+WriteBuffer(T_Buffer *Buffer,
+			char *Format,
+			...)
+{
+	SIZE_T		Bytes;
+	va_list		VarArgs;
+
+
+	va_start(VarArgs, Format);
+
+	Bytes = vsprintf(Buffer->Buff + Buffer->Pos, Format, VarArgs);
+	Buffer->Pos += Bytes;
+
+	va_end(VarArgs);
+}
+
+void
+GetFormat(CHAR *Format,
+		  CHAR *Type)
+{
+	DWORD		Hash;
+
+
+	Hash = Djb2(Type);
+	switch (Hash)
+	{
+		// Pointers
+		case TYPE_PVOID:
+		case TYPE_PSIZE_T:
+		case TYPE_LPVOID:
+		case TYPE_HANDLE:
+		case TYPE_LPSECURITY_ATTRIBUTES:
+		case TYPE_LPDWORD:
+		case TYPE_PDWORD:
+		case TYPE_PMEMORY_BASIC_INFORMATION:
+		case TYPE_PLARGE_INTEGER:
+		case TYPE_LPOVERLAPPED:
+		case TYPE_LPOVERLAPPED_COMPLETION_ROUTINE:
+		case TYPE_PLONG:
+		case TYPE_LPCVOID:
+		case TYPE_PHANDLE:
+		case TYPE_LPPROCESS_HEAP_ENTRY:
+		case TYPE_LPPROC_THREAD_ATTRIBUTE_LIST:
+		case TYPE_LPSTARTUPINFOA:
+		case TYPE_LPSTARTUPINFOW:
+		case TYPE_LPPROCESS_INFORMATION:
+		case TYPE_LPTHREAD_START_ROUTINE:
+		case TYPE_LPDEBUG_EVENT:
+		case TYPE_PBOOL:
+		case TYPE_LPBOOL:
+		case TYPE_LPRECT:
+		case TYPE_HWND:
+		case TYPE_LPPAINTSTRUCT:
+		case TYPE_HDC:
+		case TYPE_LPPOINT:
+		case TYPE_HMENU:
+		case TYPE_HINSTANCE:
+		case TYPE_LPMSG:
+		case TYPE_HBRUSH:
+		case TYPE_LPFILE:
+		{
+			strcpy(Format, "0x%p");
+		} break;
+
+		// Strings
+		case TYPE_LPSTR:
+		case TYPE_LPCSTR:
+		{
+			strcpy(Format, "\\\"%s\\\"");
+		} break;
+
+		// Unsigned
+		case TYPE_WORD:
+		case TYPE_ATOM:
+		case TYPE_DWORD:
+		case TYPE_UINT:
+		case TYPE_LRESULT:
+		case TYPE_WPARAM:
+		case TYPE_LPARAM:
+		case TYPE_LPWNDCLASSEXA:
+		case TYPE_LPWNDCLASSEXW:
+		{
+			strcpy(Format, "%u");
+		} break;
+
+		// Long unsigned
+		case TYPE_size_t:
+		case TYPE_SIZE_T:
+		{
+			strcpy(Format, "%llu");
+		} break;
+
+		// Int
+		case TYPE_int:
+		case TYPE_INT:
+		case TYPE_BOOL:
+		case TYPE_LONG:
+		case TYPE_LARGE_INTEGER:
+		case TYPE_HEAP_INFORMATION_CLASS:
+		case TYPE_short:
+		{
+			strcpy(Format, "%d");
+		} break;
+
+		// Char
+		case TYPE_CHAR:
+		{
+			strcpy(Format, "%c");
+		} break;
+
+		// Wide string
+		case TYPE_LPWSTR:
+		case TYPE_LPCWSTR:
+		{
+			strcpy(Format, "\\\"%ws\\\"");
+		} break;
+
+		default:
+		{
+			printf("CODEGEN ERR: NO FORMAT FOUND FOR %s\n", Type);
+			exit(-1);
+		} break;
+	}
+}
+
+DWORD
+Djb2(LPSTR String)
+{
+    DWORD       Hash = 5381;
+    INT         C = *String++;
+
+
+    while (C)
+    {
+        Hash = ((Hash << 5) + Hash) + C;
+        C = *String++;
+    }
+
+    return Hash;
+}
+
+void			
+GenerateHooks(T_Function *Functions,
+			  INT Count,
+			  CHAR *ListName,
+			  CHAR *Prefix,
+			  CHAR *Folder)
+{
+	INT				Len;
 	T_Buffer		SourceBuffer,
 					HeaderBuffer;
 	HANDLE 			HeaderFile,
 					SourceFile;
-	CHAR			*ListName,
-					*HeaderName,
+	CHAR			*HeaderName,
 					*SourceName,
-					*Prefix,
-					*Folder,
 					*IncludeGuard;
 	INT				Commas;
 
-
-	if (argc < 4)
-	{
-		printf("No input file provided...!\r\n");
-		printf("Usage: hookgen <file> <prefix> <folder>\r\n");
-		return (-1);
-	}
-
-	ListName = argv[1];
-	Prefix = argv[2];
-	Folder = argv[3];
 
 	Len = (INT)strlen(ListName);
 	HeaderName = (CHAR *)malloc(Len + 3);
@@ -73,7 +343,7 @@ main(int argc,
 	SourceName[Len + 1] = 'c';
 	SourceName[Len + 2] = 0;
 
-	Functions = ParseFunctions(ListName, &Count);
+	/* Functions = ParseFunctions(ListName, &Count); */
 
 	//////////////////////////////////////////////////////////////////////////
 	// Source file generation
@@ -297,241 +567,5 @@ main(int argc,
 	WriteFile(HeaderFile, HeaderBuffer.Buff, (DWORD)HeaderBuffer.Pos, 0, 0);
 	CloseHandle(HeaderFile);
 
-	return (0);
-}
-
-T_Function *
-ParseFunctions(CHAR *Filename,
-			   INT *Count)
-{
-	T_Function		*Functions;
-	CHAR			**File,
-					*Signature,
-					*Temp;
-	INT				Len;
-
-
-	File = StringFile(Filename, &Len);
-	Functions = (T_Function *)calloc(1, Len * sizeof(*Functions));
-	*Count = Len;
-
-	for (INT J = 0; J < Len; J++)
-	{
-		Signature = File[J];
-
-		// Type
-		Temp = strtok(Signature, " ,()");
-		memcpy(Functions[J].ReturnType, Temp, strlen(Temp));
-
-		// Name
-		Temp = strtok(NULL, "(");
-		memcpy(Functions[J].Name, Temp, strlen(Temp));
-
-		// Arguments
-		INT Arg = 0;
-		while (Temp != NULL)
-		{
-			Temp = strtok(NULL, " ");
-			if (!Temp)
-				break;
-			memcpy(Functions[J].ArgTypes[Arg], Temp, strlen(Temp));
-
-			Temp = strtok(NULL, ",)");
-			if (!Temp)
-				break;
-			memcpy(Functions[J].ArgNames[Arg], Temp, strlen(Temp));
-
-			Arg++;
-		}
-		Functions[J].ArgCount = Arg;
-	}
-
-	return (Functions);
-}
-
-// stb_stringfile, thank you Sean Barrett
-CHAR **
-StringFile(char *filename, int *plen)
-{
-	HANDLE				f;
-	char				*buffer, **list = NULL, *s;
-	size_t				len, count, i;
-	LARGE_INTEGER		large_int;
-
-	f = CreateFile(filename, GENERIC_READ, 0, NULL, OPEN_ALWAYS, FILE_ATTRIBUTE_NORMAL, NULL);
-	if (!f) return NULL;
-	GetFileSizeEx(f, &large_int);
-	len = large_int.QuadPart;
-	buffer = (char *)HeapAlloc(GetProcessHeap(), 0, len + 1);
-	ReadFile(f, buffer, (DWORD)len, (LPDWORD)&len, NULL);
-	buffer[len] = 0;
-	CloseHandle(f);
-
-   // two passes through: first time count lines, second time set them
-   for (i=0; i < 2; ++i) {
-      s = buffer;
-      if (i == 1)
-         list[0] = s;
-      count = 1;
-      while (*s) {
-         if (*s == '\n' || *s == '\r') {
-            // detect if both cr & lf are together
-            int crlf = (s[0] + s[1]) == ('\n' + '\r');
-            if (i == 1) *s = 0;
-            if (crlf) ++s;
-            if (s[1]) {  // it's not over yet
-               if (i == 1) list[count] = s+1;
-               ++count;
-            }
-         }
-         ++s;
-      }
-      if (i == 0) {
-         list = (char **)HeapAlloc(GetProcessHeap(), 0, sizeof(*list) * (count+1) + len+1);
-         if (!list) return NULL;
-         list[count] = 0;
-         // recopy the file so there's just a single allocation to free
-         memcpy(&list[count+1], buffer, len+1);
-         HeapFree(GetProcessHeap(), 0, buffer);
-         buffer = (char *) &list[count+1];
-         if (plen) *plen = (int) count;
-      }
-   }
-   return list;
-}
-
-void
-WriteBuffer(T_Buffer *Buffer,
-			char *Format,
-			...)
-{
-	SIZE_T		Bytes;
-	va_list		VarArgs;
-
-
-	va_start(VarArgs, Format);
-
-	Bytes = vsprintf(Buffer->Buff + Buffer->Pos, Format, VarArgs);
-	Buffer->Pos += Bytes;
-
-	va_end(VarArgs);
-}
-
-void
-GetFormat(CHAR *Format,
-		  CHAR *Type)
-{
-	DWORD		Hash;
-
-
-	Hash = Djb2(Type);
-	switch (Hash)
-	{
-		case TYPE_PVOID:
-		case TYPE_PSIZE_T:
-		case TYPE_LPVOID:
-		case TYPE_HANDLE:
-		case TYPE_LPSECURITY_ATTRIBUTES:
-		case TYPE_LPDWORD:
-		case TYPE_PDWORD:
-		case TYPE_PMEMORY_BASIC_INFORMATION:
-		case TYPE_PLARGE_INTEGER:
-		case TYPE_LPOVERLAPPED:
-		case TYPE_LPOVERLAPPED_COMPLETION_ROUTINE:
-		case TYPE_PLONG:
-		case TYPE_LPCVOID:
-		case TYPE_PHANDLE:
-		case TYPE_LPPROCESS_HEAP_ENTRY:
-		case TYPE_LPPROC_THREAD_ATTRIBUTE_LIST:
-		case TYPE_LPSTARTUPINFOA:
-		case TYPE_LPSTARTUPINFOW:
-		case TYPE_LPPROCESS_INFORMATION:
-		case TYPE_LPTHREAD_START_ROUTINE:
-		case TYPE_LPDEBUG_EVENT:
-		case TYPE_PBOOL:
-		case TYPE_LPBOOL:
-		case TYPE_LPRECT:
-		case TYPE_HWND:
-		case TYPE_LPPAINTSTRUCT:
-		case TYPE_HDC:
-		case TYPE_LPPOINT:
-		case TYPE_HMENU:
-		case TYPE_HINSTANCE:
-		case TYPE_LPMSG:
-		case TYPE_HBRUSH:
-		case TYPE_LPFILE:
-		{
-			strcpy(Format, "0x%p");
-		} break;
-
-		case TYPE_LPSTR:
-		case TYPE_LPCSTR:
-		{
-			strcpy(Format, "\\\"%s\\\"");
-		} break;
-
-		case TYPE_WORD:
-		case TYPE_ATOM:
-		case TYPE_DWORD:
-		case TYPE_UINT:
-		case TYPE_LRESULT:
-		case TYPE_WPARAM:
-		case TYPE_LPARAM:
-		case TYPE_LPWNDCLASSEXA:
-		case TYPE_LPWNDCLASSEXW:
-		{
-			strcpy(Format, "%u");
-		} break;
-
-		case TYPE_size_t:
-		case TYPE_SIZE_T:
-		{
-			strcpy(Format, "%llu");
-		} break;
-
-		case TYPE_int:
-		case TYPE_INT:
-		case TYPE_BOOL:
-		case TYPE_LONG:
-		case TYPE_LARGE_INTEGER:
-		case TYPE_HEAP_INFORMATION_CLASS:
-		case TYPE_short:
-		{
-			strcpy(Format, "%d");
-		} break;
-
-		case TYPE_CHAR:
-		{
-			strcpy(Format, "%c");
-		} break;
-
-		case TYPE_LPWSTR:
-		case TYPE_LPCWSTR:
-		{
-			strcpy(Format, "\\\"%ws\\\"");
-		} break;
-
-		default:
-		{
-			printf("CODEGEN ERR: NO FORMAT FOUND FOR %s\n", Type);
-			exit(-1);
-		} break;
-	}
-}
-
-DWORD
-Djb2(LPSTR String)
-{
-    DWORD       Hash = 5381;
-    INT         C = *String++;
-
-
-    while (C)
-    {
-        Hash = ((Hash << 5) + Hash) + C;
-        C = *String++;
-    }
-
-    return Hash;
 }
 
